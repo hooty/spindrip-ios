73cd36d912affa239a1c725e24d3730ceb1512e8
Jon Christopher <joncirca@icloud.com>
2020-07-28 17:55:04 -0700

    Initial Commit

---
+++ spindrip-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  spindrip-ios
+//
+//  Created by Jon Christopher on 7/28/20.
+//  Copyright © 2020 Spindrip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
